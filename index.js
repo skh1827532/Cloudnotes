54ca8bf564ce2f197fab122b22a8e7c8059c51ab
skh1827532 <skh1827532@gmail.com>
2022-06-07 08:33:50 +0500

    Updated files

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function showNotes() {
     let html = "";
 
     notesCont.forEach((elem, index) => {
-      html += `<div class="card mx-2 my-2" style="width: 18rem;" id=${index}>
+      html += `<div class="noteCard mx-2 my-2" style="width: 18rem;" id=${index}>
      
         <div class="card-body">
          
@@ -126,3 +126,23 @@ function updateNote() {
   let closeBtn = document.querySelector(".closeBtn");
   closeBtn.click();
 }
+
+let search = document.querySelector(".searchTxt");
+search.addEventListener("input", function () {
+  let inputVal = search.value.toLowerCase();
+  // console.log('Input event fired!', inputVal);
+  let noteCards = document.getElementsByClassName("noteCard");
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase();
+    let titleTxt = element
+      .getElementsByTagName("h5")[0]
+      .innerText.toLowerCase();
+    // console.log(cardTxt, titleTxt);
+    if (cardTxt.includes(inputVal) || titleTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+    // console.log(cardTxt);
+  });
+});
